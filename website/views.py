94dab94bf756706c9f9ad9aa45186982001ae018
muhammadfayyazkaleem <muhammadfayyazkaleem@gmail.com>
2024-02-11 04:35:37 +0500

    add response route and some functional changing

--- website/views.py
+++ website/views.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, request, render_template ,flash , redirect
+from flask import Blueprint, request, render_template ,flash , redirect, url_for
 from flask_login import login_required
 from website import db
 from .models import ContactInfo
+from flask_mail import Message
+from . import mail
 
 views = Blueprint('views', __name__)
 
-@views.route("/" , methods=['GET', 'POST'])
+@views.route("/" , methods = ['GET', 'POST'])
 def contact_form():
     if request.method == 'POST':
         name = request.form.get('name')
@@ -24,3 +26,19 @@ def contact_form():
 def admin_page():
     records = ContactInfo.query.all()
     return render_template("admin_page.html" , records = records)
+
+
+@views.route('/response/<string:email>', methods = ['POST', 'GET'])
+def reply_to_mail(email):
+   if request.method == 'POST':
+        email = request.form.get('receiver_email')
+        reply_content = request.form.get('reply_content')
+        msg = Message('Hello', sender = 'fayyazkaleem39@gmail.com', recipients = [email])
+        msg.body = reply_content
+        mail.send(msg)
+        return redirect('response.html',email=email)
+   return render_template("response.html", email=email)
+
+
+
+
