64417e269cd30f64fd6b19be99e067f76acf6b2a
muhammadfayyazkaleem <muhammadfayyazkaleem@gmail.com>
2024-02-11 04:37:07 +0500

    mailing configuration

--- website/__init__.py
+++ website/__init__.py
@@ -2,19 +2,31 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from os import path
 from flask_login import LoginManager
+from flask_mail import Mail
+
 
 
 db = SQLAlchemy()
+mail = Mail()
 db_name = "contact_info.db"
+
 
 def create_app():
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "Fayyaz.Kaleem724"
     app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{db_name}'
+    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = 'fayyazkaleem39@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'Fayyaz.Kaleem.724'
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
     
-    db.init_app(app)
 
+    mail.init_app(app)
+    db.init_app(app)
+ 
     from .views import views
     from .auth import auth
 
